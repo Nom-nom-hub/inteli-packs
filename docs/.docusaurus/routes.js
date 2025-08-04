b4c2f8c1fc97cc4495713ca345949a400cd371d0
Teck <ftwenty903@gmail.com>
2025-08-04 15:43:30 -0400

    Move documentation files out of folders for proper navbar display

--- docs/.docusaurus/routes.js
+++ docs/.docusaurus/routes.js
@@ -4,43 +4,31 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 export default [
   {
     path: '/inteli-packs/docs',
-    component: ComponentCreator('/inteli-packs/docs', 'f74'),
+    component: ComponentCreator('/inteli-packs/docs', 'bf4'),
     routes: [
       {
         path: '/inteli-packs/docs',
-        component: ComponentCreator('/inteli-packs/docs', '82a'),
+        component: ComponentCreator('/inteli-packs/docs', '1bd'),
         routes: [
           {
             path: '/inteli-packs/docs',
-            component: ComponentCreator('/inteli-packs/docs', 'a2f'),
+            component: ComponentCreator('/inteli-packs/docs', '36c'),
             routes: [
               {
-                path: '/inteli-packs/docs/features/ai-analysis',
-                component: ComponentCreator('/inteli-packs/docs/features/ai-analysis', '9ac'),
+                path: '/inteli-packs/docs/ai-analysis',
+                component: ComponentCreator('/inteli-packs/docs/ai-analysis', '8f4'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/inteli-packs/docs/features/automation',
-                component: ComponentCreator('/inteli-packs/docs/features/automation', '139'),
+                path: '/inteli-packs/docs/automation',
+                component: ComponentCreator('/inteli-packs/docs/automation', '08c'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/inteli-packs/docs/features/devops',
-                component: ComponentCreator('/inteli-packs/docs/features/devops', '48d'),
-                exact: true,
-                sidebar: "tutorialSidebar"
-              },
-              {
-                path: '/inteli-packs/docs/features/security',
-                component: ComponentCreator('/inteli-packs/docs/features/security', 'e74'),
-                exact: true,
-                sidebar: "tutorialSidebar"
-              },
-              {
-                path: '/inteli-packs/docs/features/testing',
-                component: ComponentCreator('/inteli-packs/docs/features/testing', '84e'),
+                path: '/inteli-packs/docs/devops',
+                component: ComponentCreator('/inteli-packs/docs/devops', '199'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
@@ -59,6 +47,18 @@ export default [
               {
                 path: '/inteli-packs/docs/quick-start',
                 component: ComponentCreator('/inteli-packs/docs/quick-start', 'f1f'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/inteli-packs/docs/security',
+                component: ComponentCreator('/inteli-packs/docs/security', 'f4f'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/inteli-packs/docs/testing',
+                component: ComponentCreator('/inteli-packs/docs/testing', 'd0a'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               }
